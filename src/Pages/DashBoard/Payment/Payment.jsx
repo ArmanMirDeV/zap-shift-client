4237ce2c417d9dcd3858acd98c238e7da64f4e83
Arman Mir <mirarman8583@gmail.com>
2025-11-23 07:41:54 +0600

    fetch data from db using tanstack query and use axiosSecure

--- src/Pages/DashBoard/Payment/Payment.jsx
+++ src/Pages/DashBoard/Payment/Payment.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import { useParams } from "react-router";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
 const Payment = () => {
+  const axiosSecure = useAxiosSecure();
+  const { parcelId } = useParams();
+  const { isLoading, data: parcel } = useQuery({
+    queryKey: ["parcels", parcelId],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/parcels/${parcelId}`);
+      return res.data;
+    },
+  });
+
+  if (isLoading) {
     return (
-        <div>
-            <h3>Teka Deo</h3>
-        </div>
+      <div>
+        <span className="loading loading-infinity loading-xl"></span>
+      </div>
     );
+  }
+
+  return (
+    <div>
+          <h3>Teka Deo: {parcel.parcelName}</h3>
+          <button className="btn btn-primary text-black" >Taka Dao</button>
+    </div>
+  );
 };
 
 export default Payment;
